79c893de625f59cc5030a31d5842b54c2fda472c
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-06-10 01:52:09 +0200

    add (julia.c), needs bug check

--- src/julia.c
+++ src/julia.c
@@ -6,7 +6,27 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/05 18:25:47 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/06/06 01:23:46 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/06/10 01:50:46 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fractol_utils.h"
+#include "libft.h"
+
+// return param: iterations
+// return	   : 1 or 0 is_julia
+int	is_julia(t_nbr_i z, t_nbr_i c, int iter_max, int *iters)
+{
+	double	tmp_real;
+
+	*iters = 0;
+	while (*iters < iter_max)
+	{
+		tmp_real = (z.real * z.real) - (z.imag * z.imag);
+		z.imag = (2 * z.real * z.imag) + c.imag;
+		z.real = tmp_real + c.real;
+	}
+	if (ft_abs(z.real) < 2 && ft_abs(z.imag) < 2)
+		return (1);
+	return (0);
+}

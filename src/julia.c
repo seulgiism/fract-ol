1708535b46fb3deb110a24e22e4a33148c5ac2c3
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-06-10 01:52:20 +0200

    add (julia.c), add debug todo

--- src/julia.c
+++ src/julia.c
@@ -6,13 +6,14 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/05 18:25:47 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/06/10 01:50:46 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/06/10 01:52:07 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol_utils.h"
 #include "libft.h"
 
+// TODO: BUG CHECK
 // return param: iterations
 // return	   : 1 or 0 is_julia
 int	is_julia(t_nbr_i z, t_nbr_i c, int iter_max, int *iters)

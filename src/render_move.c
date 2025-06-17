d1b5667618cc26f2782bcb046b6dff775a91afbf
Katarina Claes <kclaes@f0r3s15.codam.nl>
2025-06-17 16:35:08 +0200

    add (render_move.c), finished debugging everything completely

--- src/render_move.c
+++ src/render_move.c
@@ -6,105 +6,119 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/12 16:40:14 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/06/12 20:08:11 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/06/17 16:34:25 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42.h"
 #include "fractol_render_utils.h"
+#include "fractol_move_utils.h"
 #include "fractol.h"
 #include <math.h>
 #include <stdbool.h>
 
-static double	get_move_speed(double start, double end, bool positive, \
-								double scale);
+static double	get_move_speed(t_move move, double scale, int32_t pixels);
 
-void	move_right(t_render *render)
+void	move_right(t_render *render, int32_t pixels)
 {
 	double	step_size;
 	double	scale;
-	
+	t_move	move;
+
+	move.start = render->fract.i_start;
+	move.end = render->fract.i_end;
+	move.positive = true;
 	scale = (render->fract.i_end - render->fract.i_start) / render->img->width;
-	step_size = get_move_speed(render->fract.i_start, render->fract.i_end, true,\
-								scale);
+	step_size = get_move_speed(move, scale, pixels);
 	render->fract.i_end += step_size;
 	render->fract.i_start += step_size;
 }
 
-void	move_up(t_render *render)
+void	move_up(t_render *render, int32_t pixels)
 {
 	double	step_size;
 	double	scale;
-	
+	t_move	move;
+
+	move.start = render->fract.r_start;
+	move.end = render->fract.r_end;
+	move.positive = true;
 	scale = (render->fract.r_end - render->fract.r_start) / render->img->width;
-	step_size = get_move_speed(render->fract.r_start, render->fract.r_end, true,\
-								scale);
+	step_size = get_move_speed(move, scale, pixels);
 	render->fract.r_end += step_size;
 	render->fract.r_start += step_size;
 }
 
-void	move_left(t_render *render)
+void	move_left(t_render *render, int32_t pixels)
 {
 	double	step_size;
 	double	scale;
-	
+	t_move	move;
+
+	move.start = render->fract.i_start;
+	move.end = render->fract.i_end;
+	move.positive = false;
 	scale = (render->fract.i_end - render->fract.i_start) / render->img->width;
-	step_size = get_move_speed(render->fract.i_start, render->fract.i_end, false,\
-								scale);
+	step_size = get_move_speed(move, scale, pixels);
 	render->fract.i_end -= step_size;
 	render->fract.i_start -= step_size;
 }
 
-void	move_down(t_render *render)
+void	move_down(t_render *render, uint32_t pixels)
 {
 	double	step_size;
 	double	scale;
-	
+	t_move	move;
+
+	move.start = render->fract.r_start;
+	move.end = render->fract.r_end;
+	move.positive = true;
 	scale = (render->fract.r_end - render->fract.r_start) / render->img->width;
-	step_size = get_move_speed(render->fract.r_start, render->fract.r_end, false,\
-								scale);
-	render->fract.r_end += step_size;
-	render->fract.r_start += step_size;
+	step_size = get_move_speed(move, scale, pixels);
+	render->fract.r_end -= step_size;
+	render->fract.r_start -= step_size;
 }
 
-static double	get_move_speed(double start, double end, bool positive, double scale)
+// scale == 1 pixel in fractal depth
+static double	get_move_speed(t_move move, double scale, int32_t pixels)
 {
 	double	step_size;
-	
-	step_size = scale * MOVE_SPEED;
-	if (positive)
+
+	step_size = scale * pixels;
+	if (move.positive)
 	{
-		if (end + step_size >= MAX_FRACT)
-			step_size = MAX_FRACT - end;
+		if (move.end + step_size >= MAX_FRACT)
+			step_size = MAX_FRACT - move.end;
 	}
 	else
 	{
-		if (start - step_size <= MIN_FRACT)
-			step_size = start - MIN_FRACT;
+		if (move.start - step_size <= MIN_FRACT)
+			step_size = move.start - MIN_FRACT;
 	}
 	return (step_size);
 }
 
-#include <stdio.h>
-int	main(void)
-{
-
-	t_render	render;
-	
-	mlx_set_setting(MLX_STRETCH_IMAGE, true);
-	render.mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", true);
-	if (!render.mlx)
-		p_mlxerror();
-	render.img = mlx_new_image(render.mlx, WIDTH, HEIGHT);
-	mlx_image_to_window(render.mlx, render.img, 0, 0);
-
-	render.fract.i_start = -0.060;
-	render.fract.i_end = 0.015;
-	
-	printf("start: %.18f , end: %.18f\n", render.fract.i_start, render.fract.i_end);
-	for (int i = 0; i < 500; i++)
-	{
-		move_left(&render);
-		printf("start: %.18f , end: %.18f\n", render.fract.i_start, render.fract.i_end);
-	}
-}
+// #include <stdio.h>
+// int	main(void)
+// {
+//
+// 	t_render	render;
+//
+// 	mlx_set_setting(MLX_STRETCH_IMAGE, true);
+// 	render.mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", true);
+// 	render.img = mlx_new_image(render.mlx, WIDTH, HEIGHT);
+// 	mlx_image_to_window(render.mlx, render.img, 0, 0);
+//
+// 	render.fract.i_start = -1.75;
+// 	render.fract.i_end = 1.75;
+// 	render.fract.r_start = -1.75;
+// 	render.fract.r_end = 1.75;
+//
+// 	printf("start: %.18f , end: %.18f\n", render.fract.r_start, render.fract.r_end);
+// 	for (int i = 0; i < 1; i++)
+// 	{
+// 		move_down(&render, 30);
+// 		//move_right(&render, MOVE_SPEED);
+// 		printf("start: %.18f , end: %.18f\n", render.fract.r_start, render.fract.r_end);
+// 	}
+// }
